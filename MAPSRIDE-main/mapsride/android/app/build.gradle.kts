b7038879a25b10bd01270ecc361daa5e921e4feb
micka40380-tech <micka40380@gmail.com>
2026-07-19 17:45:53 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/app/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/app/build.gradle.kts
@@ -5,32 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mon_app"
-    compileSdk = 36
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    defaultConfig {
-        applicationId = "com.example.mon_app"
-        minSdk = flutter.minSdkVersion
-        targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
+    compileSdk = 34
 }
 
 flutter {
