a6d776833ebd7bb62c21c7c62e88805282087a29
micka40380-tech <micka40380@gmail.com>
2026-07-21 15:07:17 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/build.gradle.kts
@@ -1,26 +1,26 @@
-plugins {
-    id("com.android.application")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.micka40380.mapsride"
-    compileSdk = 36
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    defaultConfig {
-        applicationId = "com.micka40380.mapsride"
-        minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../.."
+rootProject.buildDir = java.io.File("../build")
+subprojects {
+    project.buildDir = java.io.File("${rootProject.buildDir}/${project.name}")
+}
+subprojects {
+    afterEvaluate { project ->
+        if (project.hasProperty("android")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
