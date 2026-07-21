012f23bfc62bb4fd0881971c17705d08230340ae
micka40380-tech <micka40380@gmail.com>
2026-07-21 17:59:40 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/app/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/app/build.gradle.kts
@@ -3,3 +3,33 @@ plugins {
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+android {
+    namespace = "com.micka40380.mapsride"
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.micka40380.mapsride"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    flutter {
+        source = "../.."
+    }
+}
+
+tasks.matching { it.name == "generateLockfiles" }.all {
+    enabled = false
+}
