90dbb8f97782f79826f9fb2fd1a663d57ba90c5e
micka40380-tech <micka40380@gmail.com>
2026-07-21 11:42:07 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/build.gradle.kts
@@ -6,17 +6,21 @@ allprojects {
 }
 
 subprojects {
-    afterEvaluate {
+    afterEvaluate { project ->
         val extension = extensions.findByName("android")
         if (extension is com.android.build.gradle.BaseExtension) {
             extension.namespace = extension.namespace ?: "com.google.android.gms.ads.admanager"
+            // Forçage de la version de compilation pour éviter le blocage du geocodage
+            if (project.name == "geocoding_android") {
+                extension.compileSdkVersion(34)
+            }
         }
     }
 }
 
-val newBuildDir: Directory = 
+val newBuildDir: Directory =
     rootProject.layout.buildDirectory
-        .dir("../../build")
+        .dir("../../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
