2049b77e5d357b2d33a94bcbb93cdaa2fefae939
micka40380-tech <micka40380@gmail.com>
2026-07-21 15:13:46 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/build.gradle.kts
@@ -5,22 +5,6 @@ allprojects {
     }
 }
 
-rootProject.buildDir = java.io.File("../build")
-subprojects {
-    project.buildDir = java.io.File("${rootProject.buildDir}/${project.name}")
-}
-subprojects {
-    afterEvaluate { project ->
-        if (project.hasProperty("android")) {
-            project.extensions.configure<com.android.build.gradle.BaseExtension> {
-                if (namespace == null) {
-                    namespace = project.group.toString()
-                }
-            }
-        }
-    }
-}
-
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
