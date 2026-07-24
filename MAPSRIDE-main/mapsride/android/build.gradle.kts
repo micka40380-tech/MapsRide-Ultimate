5af90f92994e6997e5634c569930eba064d5c2ad
micka40380-tech <micka40380@gmail.com>
2026-07-24 16:48:58 +0200

    Update build.gradle.kts

--- MAPSRIDE-main/mapsride/android/build.gradle.kts
+++ MAPSRIDE-main/mapsride/android/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -12,27 +16,8 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
-}
-
-subprojects {
-    fun forceSdk36() {
-        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
-            configure<com.android.build.gradle.BaseExtension> {
-                compileSdkVersion(36)
-            }
-        }
-    }
-
-    if (state.executed) {
-        forceSdk36()
-    } else {
-        afterEvaluate {
-            forceSdk36()
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
